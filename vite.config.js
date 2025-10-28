2dc9784a0d4b5c55739289dc7875524b1da60259
Tom Butler <tbutlerw@gmail.com>
2025-10-28 15:37:51 +0900

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/menu-test/',
+  server: {
+    port: 3002,
+  },
+})
+
